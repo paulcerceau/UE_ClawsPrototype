ed6a82ac392f95f560a9afcefbda55f2223adc21
Oreyato <oreyato.pro@gmail.com>
2023-10-09 18:31:12 +0200

    feat: play from here

---
+++ UE_ClawsPrototype/Source/PlayModeExtension/Public/PlayModeExtension.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+
+class FPlayModeExtensionModule : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+
+private:
+#pragma region PlayMode extension
+    //v =============================================================╗
+    //v PlayMode extension                                           ║
+    
+    void InitPlayModeExtension();
+
+    //^ PlayMode extension                                           ║
+    //^ =============================================================╝
+#pragma endregion
+};
